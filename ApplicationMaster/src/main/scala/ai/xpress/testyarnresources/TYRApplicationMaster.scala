85e8e1c62d355d8191726a5a70f4b4423dbb0c1c
Marko Nußbaum <marko.nussbaum@maconus.com>
2022-04-21 11:05:00 +0200

    ApplicationManager starts & prints out some useful info

--- ApplicationMaster/src/main/scala/ai/xpress/testyarnresources/TYRApplicationMaster.scala
+++ ApplicationMaster/src/main/scala/ai/xpress/testyarnresources/TYRApplicationMaster.scala
@@ -1,10 +1,17 @@
 package ai.xpress.testyarnresources
 
+
+import org.apache.hadoop.yarn.api.ApplicationConstants
 import org.apache.hadoop.yarn.api.records.{Container, ContainerId, ContainerLaunchContext, ContainerStatus, NodeId, NodeReport, UpdatedContainer}
 import org.apache.hadoop.yarn.client.api.async.{AMRMClientAsync, NMClientAsync}
 import org.apache.hadoop.yarn.util
 
+import java.text.SimpleDateFormat
+import java.time.format.DateTimeFormatter
+import java.time.{Instant, LocalDateTime, ZoneId}
 import java.util
+import java.util.TimeZone
+import java.util.logging.SimpleFormatter
 
 /** This is our application master (AM).
  * It handles asynchronously the execution of our application TYRMain.
@@ -26,6 +33,28 @@ object TYRApplicationMaster  {
     //nmClient = new NMClientAsync.
 
     println("Test: If you can see this, the ApplicationMaster has been started.")
+
+    val env = System.getenv()
+
+    // get & print container id
+    val containerIdString = env.get(ApplicationConstants.Environment.CONTAINER_ID.toString)
+    println("Container ID is " + containerIdString)
+
+    // get & print application submission time
+    val appTimeStampStr = env.get(ApplicationConstants.APP_SUBMIT_TIME_ENV)
+    val ts = appTimeStampStr.toLong
+    val inst = Instant.ofEpochMilli(ts)
+    val d = LocalDateTime.ofInstant(inst, ZoneId.systemDefault())
+
+    println("Application was submitted at " + d.format(DateTimeFormatter.ISO_DATE_TIME))
+
+    // get & print node manager details
+    val nmHost = env.get(ApplicationConstants.Environment.NM_HOST.toString)
+    val nmPort = env.get(ApplicationConstants.Environment.NM_PORT.toString)
+    val nmHttpPort = env.get(ApplicationConstants.Environment.NM_HTTP_PORT.toString)
+    println("NodeManager is running on host " + nmHost + ":" + nmPort.toString)
+    println("with it's web interface at http://" + nmHost + ":" + nmHttpPort.toString)
+
 
   }
 }
